142d493ca8e5739d75d706db7ff3031b59e71e64
MilenaMed <115955806+MilenaMed@users.noreply.github.com>
2023-10-09 15:50:23 -0300

    fix update/booking error  and start of the update booking test

--- src/controllers/booking-controller.ts
+++ src/controllers/booking-controller.ts
@@ -2,7 +2,6 @@ import { AuthenticatedRequest } from '@/middlewares';
 import { Response } from 'express';
 import httpStatus from 'http-status';
 import { bookingService } from '@/services';
-import { number } from 'joi';
 
 export async function getBooking(req: AuthenticatedRequest, res: Response) {
     const { userId } = req;
@@ -24,5 +23,6 @@ export async function updateBooking(req: AuthenticatedRequest, res: Response) {
     const  bookingId  = Number(req.params.bookingId)
 
     const newBooking = await bookingService.updateBooking(userId, roomId, Number(bookingId));
-    res.status(httpStatus.OK).send({ bookingId: newBooking});
+    //console.log(newBooking)
+    res.status(httpStatus.OK).send(newBooking);
 }
--- tests/integration/booking.test.ts
+++ tests/integration/booking.test.ts
@@ -228,7 +228,7 @@ describe('POST /booking', () => {
       const hotel = await createHotel();
       const room = await createRoomWithHotelId(hotel.id);
       const enrollment = await createEnrollmentWithAddress(user);
-      const ticketType = await await prisma.ticketType.create({
+      const ticketType = await prisma.ticketType.create({
         data: {
           name: faker.name.findName(),
           price: faker.datatype.number(),
@@ -241,6 +241,60 @@ describe('POST /booking', () => {
 
       expect(response.status).toEqual(httpStatus.OK);
       expect(response.body).toEqual({ bookingId: expect.any(Number) });
+    });
+  });
+});
+describe('PUT /booking/:bookingId', () => {
+  it('should respond with status 401 if no token is given', async () => {
+    const response = await server.put('/booking/1');
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+  it('should respond with status 401 if given token is not valid', async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.put('/booking/1').set('Authorization', `Bearer ${token}`).send({ roomId: 1 });
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if there is no session for given token', async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.post('/booking/1').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  describe('when token is valid', () => {
+    it('should respond with status 200 when is it updated', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await prisma.ticketType.create({
+        data: {
+          name: faker.name.findName(),
+          price: faker.datatype.number(),
+          isRemote: false,
+          includesHotel: true,
+        }
+      })
+      const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+      await createPayment(ticket.id, ticketType.price);
+
+      const hotel = await createHotel();
+      const room = await createRoomWithHotelId(hotel.id);
+      const booking = await createBooking(user.id, room.id);
+
+      const newRoom = await createRoomWithHotelId(hotel.id);
+
+      const response = await server.put(`/booking/${booking.id}`)
+      .set('Authorization', `Bearer ${token}`).send({roomId: newRoom.id});
+      expect(response.status).toEqual(httpStatus.OK);
+      expect(response.body).toEqual({
+        bookingId: booking.id,
+      });
     });
   });
 });
